83c4c87810f48bfd87431e1ed75461cc16adb32e
Buczman <mateusz.buczynski@interia.pl>
2018-01-10 02:03:16 +0100

    LML output alpha version

--- distType.m
+++ distType.m
@@ -1,10 +1,33 @@
-function distribution = distType(input, fixed, input_length)
+function distribution = distType(input, fixed, input_length, type)
 
-if nargin == 3 && fixed == 1
+if nargin == 4 && strcmp(type,'lml')
+    distribution = cell(length(input),1);
+    for i = 1:length(input)
+        switch input(i)
+            case 0
+                distribution(i,1) = {'a n'};
+            case -1
+                distribution(i,1) = {'f'};
+            case 1
+                distribution(i,1) = {'a l'};
+            case 2
+                distribution(i,1) = {'LP (n)'};
+            case 3
+                distribution(i,1) = {'LP (ln)'};
+            case 4
+                distribution(i,1) = {'Sf'};
+            case 5
+                distribution(i,1) = {'LSp'};
+            case 6
+                distribution(i,1) = {'CSp'};
+            case 7
+                distribution(i,1) = {'Pw CSp'};
+            case 8
+                distribution(i,1) = {'Pw CHISp'};
+        end
+    end
+elseif nargin == 3 && fixed == 1
     distribution = cell(input_length,1);
-%     for i = 1:input_length
-%         distribution(i,1) = {' '};
-%     end
 else
     distribution = cell(length(input),1);
     for i = 1:length(input)
@@ -29,30 +52,6 @@ else
                 distribution(i,1) = {'JSu'};
         end
     end
-    %         for j = 1:size(input,2)
-    %             for i = 1:size(input,1)
-    %                 switch input(i,j)
-    %                     case 0
-    %                         distribution(i,j) = {'n'};
-    %                     case -1
-    %                         distribution(i,j) = {'f'};
-    %                     case 1
-    %                         distribution(i,j) = {'l'};
-    %                     case 2
-    %                         distribution(i,j) = {'S'};
-    %                     case 3
-    %                         distribution(i,j) = {'t'};
-    %                     case 4
-    %                         distribution(i,j) = {'W'};
-    %                     case 5
-    %                         distribution(i,j) = {'s-a'};
-    %                     case 6
-    %                         distribution(i,j) = {'JSb'};
-    %                     case 7
-    %                         distribution(i,j) = {'JSu'};
-    %                 end
-    %             end
-    %         end
 end
 
 end
