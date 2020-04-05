0a65abb516fee3da1a032db8d2ededc6717c7b7d
Mikołaj Czajkowski <mc@uw.edu.pl>
2020-04-05 22:53:11 +0200

    Some new add-ons

--- ReplicateRows.m
+++ ReplicateRows.m
@@ -1,6 +1,14 @@
 function Y = ReplicateRows(X,no,varargin)
 
-% only tested for column vectors 
+% Y = ReplicateRows(X,no,varargin)
+% only tested for 2D matrices 
 
-Y = repmat(X,1,no)';
-Y = Y(:);
+if (size(X,2) == 1) % X is a column vector
+    Y = repmat(X,1,no)';
+    Y = Y(:);
+elseif size(X,2) == 2
+%     Y = repmat(X,[1,1,no]);
+%     Y = permute(X
+    Y = repelem(X,no,1);
+end
+    
