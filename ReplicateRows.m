ff90fb44fbb92ca38b5ab3c8329367154128e44e
Mikołaj Czajkowski <mc@uw.edu.pl>
2021-10-01 13:02:27 +0200

    Update ReplicateRows.m

--- ReplicateRows.m
+++ ReplicateRows.m
@@ -6,7 +6,7 @@ function Y = ReplicateRows(X,no,varargin)
 if (size(X,2) == 1) % X is a column vector
     Y = repmat(X,1,no)';
     Y = Y(:);
-elseif size(X,2) == 2
+elseif size(X,2) > 1
 %     Y = repmat(X,[1,1,no]);
 %     Y = permute(X
     Y = repelem(X,no,1);
